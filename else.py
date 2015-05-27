643f189063e94fa583da01c42e95fcae7cb72229
koryashkin <ae:07:3b:f6:37:f6:ed:b7:7e:aa:14:81:72:72:70:e1>
2015-05-27 15:41:08 +0900

    test
    test

---
+++ else.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+year = 1912
+if year == 1868:
+    print ("Meiji")
+else:
+    if year == 1912:
+        print("taisei")
+    else:
+       print("meiji", year-1867, "nen")
+       
